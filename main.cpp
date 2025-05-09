0cec06d99a080cd62d614036b1ba96fee7109159
Rafa N.A <rafaninoadiyatma0980@gmail.com>
2025-05-09 09:39:25 +0700

    fix bug

--- main.cpp
+++ main.cpp
@@ -5,15 +5,9 @@
 using namespace std;
 
 int main() {
-    vector<Item> item1 = {Item("Beras 5kg", 65000, 10), 
-                           Item("Gula 1kg", 15000, 20),
-                           Item("Minyak Goreng 1 Liter", 20000, 15),
-                           Item("Telur Ayam 1kg", 25000, 5),
-                           Item("Daging Sapi 1kg", 120000, 8)};
-    Market market = Market(item1);
+    Market market = Market();
     market.checkStock();
-    market.sell("Gula 1kg", 3);
-    market.sell("Minyak Goreng 1 Liter", 5);
-    market.restock("Daging Sapi 1kg", 5);
+    market.restock("Lemari", 2, 200000);
+    market.checkStock();
     return 0;
 }
--- market.h
+++ market.h
@@ -23,8 +23,23 @@ class Market {
     private:
         vector<Item> stock;
         
-    public: Market(vector<Item>stock) {
-            Market::stock = stock;
+    public: Market() {
+            Market::stock = initialize();
+        }
+
+        vector<Item> initialize() {
+            vector<Item> items = {
+                Item("Beras 5kg", 65000, 10), 
+                Item("Gula 1kg", 15000, 20),
+                Item("Minyak Goreng 1 Liter", 20000, 15),
+                Item("Telur Ayam 1kg", 25000, 5),
+                Item("Daging Sapi 1kg", 120000, 8),
+                Item("Susu UHT 1 Liter", 15000, 12),
+                Item("Kecap Manis 500ml", 10000, 25),
+                Item("Mie Instan", 3000, 50),
+                Item("Teh Celup 25pcs", 12000, 30),
+                Item("Sabun Mandi 100gr", 5000, 40),};
+            return items;
         }
 
         void checkStock() {
@@ -47,8 +62,8 @@ class Market {
                         cout << "Stock not enough" << endl;
                         return;
                     } else{
-                        stock[i].quantity - amount;
-                        cout << stock[i].name << " has been sell: " << amount << endl
+                        stock[i].quantity -= amount;
+                        cout <<  stock[i].name<< " has been sell: " << amount << endl
                         << "Total harga: " << stock[i].price * amount << endl;
                         return;
                     }
@@ -57,16 +72,19 @@ class Market {
             cout << "Please enter right product name!" << endl;
         }
 
-        void restock(string name,int amount) {
-            for(int i = 0; i < stock.size(); i++) {
-                if(name == stock[i].name) {
-                    stock[i].quantity + amount;
-                    cout << stock[i].name << " succesfull restocked: " << amount << endl
-                    << "Total stock on " << stock[i].name << " : " << stock[i].quantity << endl;
-                    return;
+        void restock(string name,int amount, int price = 0) {
+            if(price == 0) {
+                for(int i = 0; i < stock.size(); i++) {
+                    if(name == stock[i].name) {
+                        stock[i].quantity += amount;
+                        cout << stock[i].name << " succesfull restocked: " << amount << endl
+                        << "Total stock on " << stock[i].name << " : " << stock[i].quantity << endl;
+                        return;
+                    }
                 }
             }
-            cout << "Please enter right product name!" << endl;
+            Item newItem = Item(name, price, amount);
+            stock.push_back(newItem);
         }
 };
 #endif
